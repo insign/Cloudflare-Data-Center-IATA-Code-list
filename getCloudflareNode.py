99d6c1d2538e51e2a24cddfe23dcb6e1ea137f53
LufsX <lufsx@outlook.com>
2024-08-18 20:09:05 +0800

    update: new node

--- getCloudflareNode.py
+++ getCloudflareNode.py
@@ -2,7 +2,7 @@ import requests
 import json
 import re
 
-# 从 Cloudflare 节点状态中获取，目前（2024-07-13）共 329 个
+# 从 Cloudflare 节点状态中获取，目前（2024-08-18）共 333 个
 url = "https://www.cloudflarestatus.com/api/v2/components.json"
 
 response = requests.get(url)
